024cbb522a18b441bfa408944f3ff19c65241c6d
Konstantin <5023988+kpermyakov@users.noreply.github.com>
2023-04-18 22:30:50 +0300

    first commit

---
+++ src/test/java/ru/nrb/cbr/xmljoinerdelta/XmlJoinerDeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nrb.cbr.xmljoinerdelta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlJoinerDeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
